1ddbbb5991c1b3af0f64a97b9e447fbc4153fc7e
shteky <neversoftbg@yahoo.com>
2024-10-14 19:48:45 +0300

    push all

---
+++ cypress/support/DemoQA/steps.js
@@ -0,0 +1,11 @@
+const helpers = require('../support/functions')
+const assert = require('../assertions')
+const homepage = require('../DemoQA/Selectors/homepage')
+const bookstore = require('../DemoQA/Selectors/bookstore')
+
+
+
+
+module.exports = { 
+
+}
